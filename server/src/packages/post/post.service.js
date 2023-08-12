239548faa3d48764f27f95894339dd3d989601f5
catmika <maxand2000@gmail.com>
2023-08-12 16:51:57 +0300

    thjs-1: * post service and post-reaction repo

--- server/src/packages/post/post.service.js
+++ server/src/packages/post/post.service.js
@@ -19,27 +19,34 @@ class PostService {
     });
   }
 
-  async setReaction(userId, { postId, isLike = true }) {
-    // define the callback for future use as a promise
-    const updateOrDelete = react => {
-      return react.isLike === isLike
-        ? this._postReactionRepository.deleteById(react.id)
-        : this._postReactionRepository.updateById(react.id, { isLike });
-    };
+  async setReaction(userId, { postId, isLike = true, isDislike = false }) {
+    if (isLike && isDislike) {
+      throw new Error('Cannot set both isLike and isDislike to true.');
+    }
 
-    const reaction = await this._postReactionRepository.getPostReaction(
+    const existingReaction = await this._postReactionRepository.getPostReaction(
       userId,
       postId
     );
 
-    const result = reaction
-      ? await updateOrDelete(reaction)
-      : await this._postReactionRepository.create({ userId, postId, isLike });
+    if (existingReaction) {
+      (isLike && existingReaction.isLike) ||
+      (isDislike && existingReaction.isDislike)
+        ? await this._postReactionRepository.deleteById(existingReaction.id)
+        : await this._postReactionRepository.updateById(existingReaction.id, {
+            isLike,
+            isDislike
+          });
+    } else {
+      await this._postReactionRepository.create({
+        userId,
+        postId,
+        isLike,
+        isDislike
+      });
+    }
 
-    // the result is an integer when an entity is deleted
-    return Number.isInteger(result)
-      ? {}
-      : this._postReactionRepository.getPostReaction(userId, postId);
+    return await this._postReactionRepository.getReactionCount(postId);
   }
 }
 
